d747725e67c875cce66e8ccb89aadfb43a0062c0
Mark Mulli <markmulli001@gmail.com>
2023-09-02 12:56:43 +0300

    Angular Authorization UI

---
+++ src/app/components/signup/signup.component.ts
@@ -0,0 +1,65 @@
+import { Component , OnInit} from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-signup',
+  templateUrl: './signup.component.html',
+  styleUrls: ['./signup.component.scss']
+})
+export class SignupComponent implements OnInit{
+
+  type: string = "password";
+  isText: boolean = false;
+  eyeIcon: string = "fa-eye-slash";
+  signUpForm! : FormGroup;
+
+  constructor(private fb: FormBuilder){
+
+  }
+  ngOnInit(): void {
+    this.signUpForm = this.fb.group({
+      firstname: ['',Validators.required],
+      lastname: ['',Validators.required],
+      email: ['',Validators.required],
+      username: ['',Validators.required],
+      password: ['',Validators.required]
+
+    })
+
+  }
+  hideShowPass() {
+    this.isText = !this.isText;
+    this.isText ? this.eyeIcon = 'fa-eye' : this.eyeIcon = 'fa-eye-slash';
+    this.isText ? this.type = "text" : this.type = "password";
+
+  }
+
+  onSignUp() {
+    if(this.signUpForm.valid) {
+
+    }
+    else {
+
+      this.validateFormFields(this.signUpForm);
+      alert("Your form is invalid");
+
+    }
+
+  }
+
+  private validateFormFields(formGroup: FormGroup) {
+    Object.keys(formGroup.controls).forEach(field =>{
+
+      const control = formGroup.get(field)
+
+      if (control instanceof FormControl) {
+        control.markAsDirty({ onlySelf: true });
+      }
+      else if( control instanceof FormGroup ) {
+        this.validateFormFields(control)
+      }
+
+    })
+  }
+
+}
