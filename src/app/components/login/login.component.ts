5851760c9cb9c5a8533580cd646ff255d8777104
Mark Mulli <markmulli001@gmail.com>
2023-09-07 17:17:58 +0300

    updates on angular UI

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -31,8 +31,18 @@ export class LoginComponent implements OnInit{
 
   onLogin() {
     if (this.loginForm.valid) {
+      this.auth.login(this.loginForm.value)
+      .subscribe({
+        next: (res) => {
+          alert(res.message)
+        },
+        error: (err) => {
+          alert(err?.error.message)
+        }
 
+      })
     }
+
     else {
       this.validateFormFields(this.loginForm);
       alert('Your form is invalid')
--- src/app/components/signup/signup.component.ts
+++ src/app/components/signup/signup.component.ts
@@ -1,5 +1,6 @@
 import { Component , OnInit} from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-signup',
@@ -13,7 +14,7 @@ export class SignupComponent implements OnInit{
   eyeIcon: string = "fa-eye-slash";
   signUpForm! : FormGroup;
 
-  constructor(private fb: FormBuilder){
+  constructor(private fb: FormBuilder, private auth: AuthService){
 
   }
   ngOnInit(): void {
@@ -36,6 +37,17 @@ export class SignupComponent implements OnInit{
 
   onSignUp() {
     if(this.signUpForm.valid) {
+
+      this.auth.signUp(this.signUpForm.value)
+      .subscribe({
+        next: (res) => {
+          alert(res.message)
+          this.signUpForm.reset()
+        },
+        error: (err) => {
+          alert(err?.error.message)
+        }
+      })
 
     }
     else {
