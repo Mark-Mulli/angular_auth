996a320f7076169006f9f53b58fd256ba7b96eb2
Mark Mulli <markmulli001@gmail.com>
2023-09-12 12:30:34 +0300

    authentication guards

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,16 +1,19 @@
-import {CanActivate} from '@angular/router';
+import {CanActivate, Router} from '@angular/router';
 import { AuthService } from '../services/auth.service';
 import { Injectable } from '@angular/core';
+import { NgToastService } from 'ng-angular-popup';
 
 
 export class authGuard implements CanActivate  {
-  constructor(private auth: AuthService){}
+  constructor(private auth: AuthService,private router: Router, private toast: NgToastService){}
 
   canActivate(): boolean  {
     if (this.auth.isLoggedIn()) {
       return true;
     }
     else {
+      this.toast.error({detail:"ERROR",summary:"Please Login First!"})
+      this.router.navigate(['login'])
       return false;
     }
   }
