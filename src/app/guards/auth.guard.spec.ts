e63726f550c951b52bbf0c5fb1b5e74e9d45126e
Mark Mulli <markmulli001@gmail.com>
2023-09-12 15:56:08 +0300

    implementing guards

--- src/app/guards/auth.guard.spec.ts
+++ src/app/guards/auth.guard.spec.ts
@@ -1,41 +1,16 @@
 import { TestBed } from '@angular/core/testing';
-import { ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
-import { AuthGuard } from './auth.guard'; // Import the AuthGuard class
+
+import { AuthGuard } from './auth.guard';
 
 describe('AuthGuard', () => {
   let guard: AuthGuard;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
-    guard = TestBed.inject(AuthGuard); // Inject the AuthGuard class
+    guard = TestBed.inject(AuthGuard);
   });
 
   it('should be created', () => {
     expect(guard).toBeTruthy();
-  });
-
-  it('should allow access when authenticated', () => {
-    // You can mock your AuthService isAuthenticated() method here
-    spyOn(guard['authService'], 'isLoggedIn').and.returnValue(true);
-
-    const canActivate = guard.canActivate(
-      {} as ActivatedRouteSnapshot,
-      {} as RouterStateSnapshot
-    );
-
-    expect(canActivate).toBe(true);
-  });
-
-  it('should redirect to login when not authenticated', () => {
-    // You can mock your AuthService isAuthenticated() method here
-    spyOn(guard['authService'], 'isLoggedIn').and.returnValue(false);
-
-    const canActivate = guard.canActivate(
-      {} as ActivatedRouteSnapshot,
-      {} as RouterStateSnapshot
-    );
-
-    // Expect canActivate to be a UrlTree that redirects to /login
-    expect(canActivate.toString()).toContain('/login');
   });
 });
--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,24 +1,23 @@
+import { AuthService } from './../services/auth.service';
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
-import { AuthService } from '../services/auth.service';
-import { Observable } from 'rxjs';
+import { CanActivate, Router } from '@angular/router';
+import { NgToastService } from 'ng-angular-popup';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
+  constructor(private auth : AuthService, private router: Router, private toast: NgToastService){
 
-  constructor(private authService: AuthService, private router: Router) {}
-
-  canActivate(
-    route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot
-  ): boolean | UrlTree | Observable<boolean | UrlTree> | Promise<boolean | UrlTree> {
-    if (this.authService.isLoggedIn()) {
-      return true;
-    } else {
-      // Redirect to the login page if not authenticated
-      return this.router.createUrlTree(['/login']);
+  }
+  canActivate():boolean{
+    if(this.auth.isLoggedIn()){
+      return true
+    }else{
+      this.toast.error({detail:"ERROR", summary:"Please Login First!"});
+      this.router.navigate(['login'])
+      return false;
     }
   }
+
 }
